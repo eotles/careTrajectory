64ee8331d6b35353a0499fe678e3950c52c60713
eotles <eotles@gmail.com>
2016-07-08 11:29:46 -0500

    clean up and rename

--- careTrajectory.py
+++ careTrajectory.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import multiprocessing as mp
+import matplotlib
+import matplotlib.pyplot as plt
 
 #globals
 _pt_col='ptID'
@@ -58,7 +60,7 @@ def calc_enc_traj(pt_lookup, pt_trajs, index_enc_id):
         enc_traj['index'].append(index_enc_id)
     return(enc_traj)
 
-def make_enc_traj_df(data, pt_col='MRN', enc_col='CSN', date_col='date', lim=None):
+def main(data, pt_col='MRN', enc_col='CSN', date_col='date', lim=None):
     set_globals(pt_col, enc_col, date_col)
     pt_trajs = traj_extract_pl(data, lim=lim)
     pt_lookup = data[data[pt_col].isin(pt_trajs.keys())]
@@ -73,10 +75,60 @@ def make_enc_traj_df(data, pt_col='MRN', enc_col='CSN', date_col='date', lim=Non
         all_enc_trajs[key] = [item for enc_pile in enc_trajs for item in enc_pile[key]]
     
     enc_traj_df = pd.DataFrame(all_enc_trajs)
-    return(enc_traj_df)
+    extract = traj_extract(enc_traj_df, pt_col, enc_col, date_col)
+    return(extract)
 
-def main():
-    pass
+class traj_extract():
+    def __init__(self, df, pt_col='MRN', enc_col='CSN', date_col='date'):
+        self.df = df
+        self.pt_col = pt_col
+        self.enc_col = enc_col
+        self.date_col = date_col
+    
+    def _curve_counter(self, idxs):
+        traj = self.df[self.df['index'].isin(idxs)]
+        window = self.df['offset'].abs().max()
+        
+        pts_on_day = {}
+        for i in xrange(-window,window+1):
+            pts_on_day[i] = list(traj[traj['offset']==i][self.pt_col].unique())
+        
+        pts_middle_out = {i: pts_on_day[i] for i in [-1, 0, 1]}
+        for offset in xrange(2, window+1):
+            for sense in [-1,1]:
+                curr = offset*sense
+                prev = curr-sense
+                pts_middle_out[curr] = pts_middle_out[prev] + pts_on_day[curr]
+        pts_middle_out = {k: set(v) for k,v in pts_middle_out.iteritems()}
+        
+        pts_cnt_middle_out = {i: len(pt_list) for i,pt_list in pts_middle_out.iteritems()}
+        curve = {i: pts_cnt_middle_out[0]-v for i,v in pts_cnt_middle_out.iteritems()}
+        curve[0] = pts_cnt_middle_out[0]
+        
+        return(curve)
+    
+    def plot_fig(self, curve_df):
+        #curve = self._curve_counter(idxs)
+        #curve = pd.Series(curve)
+        curve_df[0] = curve_df[0] / curve_df[0].max()
+        plt.figure(1)
+        plt.figure(figsize = (7,3))
+        gs1 = matplotlib.gridspec.GridSpec(3, 7)
+        gs1.update(wspace=0, hspace=0.05)
+        
+        ax1 = plt.subplot(gs1[:,0:3])
+        curve_df[0].loc[:-1].plot(ylim=[0,1])
+        
+        ax2 = plt.subplot(gs1[:,3], sharey=ax1)
+        curve_df_gb = curve_df.groupby('Admit').sum()
+        curve_df_gb[0] = curve_df_gb[0] / curve_df_gb[0].sum()
+        curve_df_gb[0].plot(kind='bar', stacked=True, ylim=[0,1])
+        
+        ax3 = plt.subplot(gs1[:,4:7], sharey=ax1)
+        curve_df[0].loc[1:].plot(ylim=[0,1])
+        
+        plt.show()
+
 
 if __name__ == '__main__':
     main()
